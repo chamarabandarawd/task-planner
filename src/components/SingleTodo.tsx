8b805e51b0c97422ad2304b33e6ec25399b5ff81
chamarabandarawd <chamarabandarawd@gmail.com>
2023-08-22 14:13:02 +0530

    taske-done status completed

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ToDo } from '../model';
-import{ AiOutlineEdit,AiOutlineDelete ,AiOutlineFileDone} from "react-icons/ai"
+import{ AiOutlineEdit,AiOutlineDelete ,AiOutlineFileDone} from "react-icons/ai";
+import "./styles.css";
 
 interface Props {
     todo:ToDo;
@@ -8,16 +9,33 @@ interface Props {
     setTodos:React.Dispatch<React.SetStateAction<ToDo[]>>
 }
 
+
+
 const SingleTodo:React.FC<Props> = ({todo,todos,setTodos}:Props) => {
+
+  const handleDone=(id:number)=>{
+    setTodos(todos.map((todo)=>(
+      todo.id===id?{...todo,isDone:!todo.isDone}:todo
+    )));
+  
+  };
+
+  const handleDelete=(id:number)=>{
+    setTodos(todos.filter((todo)=>todo.id!==id));
+  };
+
   return (
     <form className='todos_single'>
-        <span className="todos_single-text">
-            {todo.todo}
-            </span>
-            <div>
+       {todo.isDone?(
+        <s className="todos_single-text">{todo.todo}</s>
+       ):(
+        <span className="todos_single-text">{todo.todo}</span>
+       )}
+            
+            <div className='icon-main'>
                 <span className="icon"><AiOutlineEdit/></span>
-                <span className="icon"><AiOutlineDelete/></span>
-                <span className="icon"><AiOutlineFileDone/></span>
+                <span className="icon" onClick={()=>handleDelete(todo.id)}><AiOutlineDelete/></span>
+                <span className="icon" onClick={()=>handleDone(todo.id)}><AiOutlineFileDone/></span>
             </div>
     </form>
   )
